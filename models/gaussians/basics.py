c01f5a3ae95e614f49f07cfb9258e4eb1badf88e
VincentMa123 <vincentma19@yahoo.com>
2026-05-14 00:24:32 +0000

    fix keyerror

--- models/gaussians/basics.py
+++ models/gaussians/basics.py
@@ -170,9 +170,12 @@ def remove_from_optim(optimizer, deleted_mask, param_dict):
             param_state = optimizer.state[old_params]
             del optimizer.state[old_params]
 
-            # Modify the state directly without deleting and reassigning.
-            param_state["exp_avg"] = param_state["exp_avg"][~deleted_mask]
-            param_state["exp_avg_sq"] = param_state["exp_avg_sq"][~deleted_mask]
+            # Adam only initializes moments after a parameter receives a
+            # gradient. Some auxiliary Gaussian attributes can be optimized
+            # structurally before they ever receive gradients.
+            if "exp_avg" in param_state and "exp_avg_sq" in param_state:
+                param_state["exp_avg"] = param_state["exp_avg"][~deleted_mask]
+                param_state["exp_avg_sq"] = param_state["exp_avg_sq"][~deleted_mask]
 
             # Update the parameter in the optimizer's param group.
             del optimizer.param_groups[group_idx]["params"][0]
@@ -188,18 +191,19 @@ def dup_in_optim(optimizer, dup_mask, param_dict, n=2):
             old_params = group["params"][0]
             new_params = param_dict[name]
             param_state = optimizer.state[old_params]
-            repeat_dims = (n,) + tuple(1 for _ in range(param_state["exp_avg"].dim() - 1))
-            param_state["exp_avg"] = torch.cat(
-                [param_state["exp_avg"], torch.zeros_like(param_state["exp_avg"][dup_mask.squeeze()]).repeat(*repeat_dims)],
-                dim=0,
-            )
-            param_state["exp_avg_sq"] = torch.cat(
-                [
-                    param_state["exp_avg_sq"],
-                    torch.zeros_like(param_state["exp_avg_sq"][dup_mask.squeeze()]).repeat(*repeat_dims),
-                ],
-                dim=0,
-            )
+            if "exp_avg" in param_state and "exp_avg_sq" in param_state:
+                repeat_dims = (n,) + tuple(1 for _ in range(param_state["exp_avg"].dim() - 1))
+                param_state["exp_avg"] = torch.cat(
+                    [param_state["exp_avg"], torch.zeros_like(param_state["exp_avg"][dup_mask.squeeze()]).repeat(*repeat_dims)],
+                    dim=0,
+                )
+                param_state["exp_avg_sq"] = torch.cat(
+                    [
+                        param_state["exp_avg_sq"],
+                        torch.zeros_like(param_state["exp_avg_sq"][dup_mask.squeeze()]).repeat(*repeat_dims),
+                    ],
+                    dim=0,
+                )
             del optimizer.state[old_params]
             optimizer.state[new_params[0]] = param_state
             optimizer.param_groups[group_idx]["params"] = new_params
